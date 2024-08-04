52447f13e53e4c1db5d527325f490f628123a4f2
Oskar <hamerlao@gmail.com>
2024-08-04 13:11:40 +0200

    Created the service and controller for employees

---
+++ ManagmentApp/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using ManagmentApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagmentApp.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public ActionResult<IEnumerable<Employee>> GetAll()
+        {
+
+        }
+    }
+
+    
+}
---
+++ ManagmentApp/Services/EmployeeService.cs
@@ -0,0 +1,28 @@
+﻿using ManagmentApp.Models;
+using ManagmentApp.Repositories;
+
+namespace ManagmentApp.Services
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<Employee>> GetAllEmployeesAsync();
+    }
+
+    public class EmployeeService : IEmployeeService 
+    {
+        private readonly EmployeeRepo _employeeRepo;
+
+        public EmployeeService(EmployeeRepo employeeRepo)
+        {
+            _employeeRepo = employeeRepo;
+        }
+
+
+        public async Task<IEnumerable<Employee>> GetAllEmployeesAsync()
+        {
+            var employees = await _employeeRepo.GetAllAsync();  
+
+            return employees;
+        }
+    }
+}
