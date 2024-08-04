1aa010ab47556e7081d748e36050daf39a09f956
Oskar <hamerlao@gmail.com>
2024-08-04 12:49:33 +0200

    CRUD operation added

--- ManagmentApp/Repositories/EmployeeRepo.cs
+++ ManagmentApp/Repositories/EmployeeRepo.cs
@@ -8,9 +8,32 @@ namespace ManagmentApp.Repositories
     public class EmployeeRepo
     {
         private readonly IMongoCollection<Employee> _employeeCollection;
+        private const string COLLECTION_NAME = "Employees";
         public EmployeeRepo(IOptions<EmployeeSystemSettings> options)
         {
-            
+            var mongoClient = new MongoClient(
+            options.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                options.Value.DataBaseName);
+
+            _employeeCollection = mongoDatabase.GetCollection<Employee>(COLLECTION_NAME);
+             
         }
+        public async Task<List<Employee>> GetAsync() =>
+        await _employeeCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Employee?> GetAsync(string id) =>
+            await _employeeCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Employee newEmp) =>   
+            await _employeeCollection.InsertOneAsync(newEmp);
+            
+        public async Task UpdateAsync(string id, Employee updateEmp) =>
+            await _employeeCollection.ReplaceOneAsync(x => x.Id == id, updateEmp);
+
+        public async Task RemoveAsync(string id) =>
+            await _employeeCollection.DeleteOneAsync(x => x.Id == id);
+
     }
 }
