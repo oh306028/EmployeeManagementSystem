37a1894d0bcb2cc7f9ae0a964e08f92ce8ba51d8
Oskar <hamerlao@gmail.com>
2024-08-04 12:32:51 +0200

    Created the employee model and repository for holding the CRUD operations for emplyees

---
+++ ManagmentApp/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ManagmentApp.Models
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        public string Email { get; set; }   
+    }
+}
---
+++ ManagmentApp/Repositories/EmployeeRepo.cs
@@ -0,0 +1,15 @@
+﻿using ManagmentApp.DbStorage;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ManagmentApp.Repositories
+{
+    public class EmployeeRepo
+    {
+        private readonly IMongoCollection<Employee> _employeeCollection;
+        public EmployeeRepo(IOptions<EmployeeSystemSettings> options)
+        {
+            
+        }
+    }
+}
