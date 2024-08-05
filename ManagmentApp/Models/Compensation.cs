3e6194bb9578720413f9d314777aede4a1461339
Oskar <hamerlao@gmail.com>
2024-08-06 00:23:53 +0200

    Created the compensation entity

---
+++ ManagmentApp/Models/Compensation.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ManagmentApp.Models
+{
+    public class Compensation
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public double SalaryPerMonth { get; set; }
+
+        public double BonusPerMonth { get; set; }   
+
+        public string EmployeeId { get; set; }  
+    }
+}
