d4696d9cf17dea6372619468683beed86aa42f37
Oskar <hamerlao@gmail.com>
2024-08-06 00:24:18 +0200

    Created the 'join' model for employee with compensation and departaments information

---
+++ ManagmentApp/Models/EmployeeWithDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace ManagmentApp.Models
+{
+    public class EmployeeWithDetails
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+        public string DepartamentName { get; set; }
+        public string Email { get; set; }
+        public double SalaryPerMonth { get; set; }
+        public double BonusPerMonth { get; set; }
+    }
+}
