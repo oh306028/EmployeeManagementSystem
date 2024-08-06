ee65efd0d59a804afa97a6cdf38174535f6d6db6
Oskar <hamerlao@gmail.com>
2024-08-06 17:27:34 +0200

    created the middleware for handling exceptions; created notFound ex

---
+++ ManagmentApp/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿
+using ManagmentApp.Exceptions;
+
+namespace ManagmentApp.Middlewares
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+               await next.Invoke(context);
+            }catch(NotFoundException notFound) 
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFound.Message);
+            }
+            catch(Exception ex)
+            {
+                context.Response.StatusCode = 400;
+               await context.Response.WriteAsync(ex.Message);
+            }
+          
+        }
+    }
+}
