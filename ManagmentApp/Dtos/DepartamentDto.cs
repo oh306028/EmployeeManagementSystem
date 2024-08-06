8d2f7d8a2181c6643399b6c5004b8088f9697a1d
Oskar <hamerlao@gmail.com>
2024-08-06 10:44:15 +0200

    Refactored the dtos for displaying departaments with emp

---
+++ ManagmentApp/Dtos/DepartamentDto.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ManagmentApp.Dtos
+{
+    public class DepartamentDto
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<EmployeeDto> Employees{ get; set; } 
+    }
+}
---
+++ ManagmentApp/Dtos/EmployeeDto.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ManagmentApp.Dtos
+{
+    public class EmployeeDto
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
